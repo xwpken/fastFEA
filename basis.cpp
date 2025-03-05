227c6335f01da642b50a8c786e0805b46da30f3f
xwpken <weipeng@BigMac.local>
2025-03-05 13:50:54 +0800

    first commit

---
+++ basis.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int a = 10;
+    
+    auto name = std::string{};
+    std::cin >> name;
+
+    std::cout << name;
+
+    printf("Hello World\n"); 
+    printf("Are you OK?"); 
+    return 0;
+}
+ 
