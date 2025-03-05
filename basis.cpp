21b39c2cfedd3ec58f09f5ba12e572ebc0398911
xwpken <me.wxu@outlook.com>
2025-03-05 14:18:50 +0800

    test in WSL

--- basis.cpp
+++ basis.cpp
@@ -6,14 +6,17 @@ using namespace std;
 int main()
 {
     int a = 10;
+    string info = "Please input your name...";
+
+    std::cout << info << endl;
     
     auto name = std::string{};
     std::cin >> name;
 
-    std::cout << name;
+    std::cout << name << endl;
 
     printf("Hello World\n"); 
-    printf("Are you OK?"); 
+    printf("Are you OK?\n"); 
     return 0;
 }
  
